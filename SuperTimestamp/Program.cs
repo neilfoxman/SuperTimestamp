8845516c72c1530d6fa0ade7b084a899af6a3684
neil <neilfoxman@gmail.com>
2018-02-12 21:24:24 -0800

    much progress on scanning for file info

--- SuperTimestamp/Program.cs
+++ SuperTimestamp/Program.cs
@@ -11,10 +11,14 @@ namespace SuperTimestamp
 {
     class Program
     {
+
+        public static bool DEBUGGING = true;
+        public enum FTYPE { DIR, FILE};
+
         static void Main(string[] args)
         {
             // Verify argument(s) (file path) were supplied
-            if(args.Length < 1)
+            if (args.Length < 1)
             {
                 //Console.WriteLine("No path supplied.  Exiting.");
                 //Console.ReadKey();
@@ -22,110 +26,214 @@ namespace SuperTimestamp
             }
 
 
+
+
             // Get original path
-            string origPath = args[0];
-            for(int argidx = 1; argidx < args.Length; argidx++)
+            string targetPathPrev = args[0];
+            for (int argidx = 1; argidx < args.Length; argidx++)
             {
-                origPath += " " + args[argidx];
+                targetPathPrev += " " + args[argidx];
+            }
+            DebugOutput("targetPathPrev:" + targetPathPrev);
+
+
+
+
+
+
+
+
+            // https://stackoverflow.com/questions/1395205/better-way-to-check-if-a-path-is-a-file-or-a-directory
+            // get the file attributes for file or directory
+            FileAttributes attr = File.GetAttributes(targetPathPrev);
+            //detect whether its a directory or file
+            FTYPE ftype;
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                //Its a directory
+                ftype = FTYPE.DIR;
+            }
+            else
+            {
+                ftype = FTYPE.FILE;
             }
 
-            //// For Debug
-            //Console.Write("Original path is: \t" + origPath + '\t');
-            //foreach (char c in origPath)
-            //{
-            //    Console.Write(Convert.ToInt32(c) + " ");
-            //}
-            //Console.WriteLine();
+
+
+
 
 
             // Strip info from path
-            string fExt = Path.GetExtension(origPath);
-            string fName = Path.GetFileNameWithoutExtension(origPath);
-            string dirName = Path.GetDirectoryName(origPath);
+            string targetDirName = Path.GetDirectoryName(targetPathPrev);
+            string targetNamePrev = Path.GetFileNameWithoutExtension(targetPathPrev);
+            string targetExt = Path.GetExtension(targetPathPrev);
+
+            // Strip backslash off drive letter paths - only does for drive letter paths for some reason
+            if (targetDirName[targetDirName.Length - 1] == '\\')
+            {
+                DebugOutput("Unnecessary backslash found in directory.");
+                DebugOutput("targetDirName Was:" + targetDirName);
+                targetDirName = targetDirName.Substring(0, targetDirName.Length - 1);
+            }
 
             // For Debugging
-            Console.WriteLine("Extension:" + fExt);
-            Console.WriteLine("Name:" + fName);
-            Console.WriteLine("DirectoryName:" + dirName);
+            DebugOutput("targetDirName:" + targetDirName);
+            DebugOutput("targetNamePrev:" + targetNamePrev);
+            DebugOutput("targetExt:" + targetExt);
+
+
+
+
 
             // Strip Info from Name
             // we are searching for characters from right to left so it's easiest to do it piecewise
-            string rootName = "";
-            string timestamp = "";
-            string timestampIncrement = "";
+            string targetNameRoot = "";
+            string targetNameTimestampPrev = "";
+            string targetNameIncrementPrev = "";
 
-            // Search for presence of timestamp and increment
-            Regex regexTimestampAndIncrement = new Regex(@"(.*)_(\d{8})_(\d*)"); // Regex for comparison
-            Match matchTimestampAndIncrement = regexTimestampAndIncrement.Match(fName); // Match object to store matches
-            if (matchTimestampAndIncrement.Success) // If normal timestampand increment are present
+            // Search for presence of targetNameTimestampPrev and increment
+            Regex regexTimestampIncrement = new Regex(@"(.*)_(\d{8})_(\d*)"); // Regex for comparison
+            Match matchTimestampIncrement = regexTimestampIncrement.Match(targetNamePrev); // Match object to store matches
+            if (matchTimestampIncrement.Success) // If normal timestampand increment are present
             {
-                rootName = matchTimestampAndIncrement.Groups[1].ToString();
-                timestamp = matchTimestampAndIncrement.Groups[2].ToString();
-                timestampIncrement = matchTimestampAndIncrement.Groups[3].ToString();
+                targetNameRoot = matchTimestampIncrement.Groups[1].ToString();
+                targetNameTimestampPrev = matchTimestampIncrement.Groups[2].ToString();
+                targetNameIncrementPrev = matchTimestampIncrement.Groups[3].ToString();
 
             }
             else
             {
                 // See if there's a date but no increment
                 Regex regexTimestamp = new Regex(@"(.*)_(\d{8})");
-                Match matchTimestamp = regexTimestamp.Match(fName);
+                Match matchTimestamp = regexTimestamp.Match(targetNamePrev);
                 if (matchTimestamp.Success)
                 {
-                    rootName = matchTimestamp.Groups[1].ToString();
-                    timestamp = matchTimestamp.Groups[2].ToString();
+                    targetNameRoot = matchTimestamp.Groups[1].ToString();
+                    targetNameTimestampPrev = matchTimestamp.Groups[2].ToString();
                 }
                 else
                 {
-                    rootName = fName;
+                    targetNameRoot = targetNamePrev;
                 }
             }
-
-            //// For Debugging
-            //Console.WriteLine("Root Name:" + rootName);
-            //Console.WriteLine("Timestamp:" + timestamp);
-            //Console.WriteLine("Increment:" + timestampIncrement);
+            DebugOutput("targetNameRoot:" + targetNameRoot);
+            DebugOutput("targetNameTimestampPrev:" + targetNameTimestampPrev);
+            DebugOutput("targetNameIncrementPrev:" + targetNameIncrementPrev);
 
 
 
 
 
 
-            //// Get date
-            //DateTime localDate = DateTime.Now;
-            //string strLocalDate = localDate.ToString("yyyyMMdd");
+            // Get new date
+            DateTime dtNewDate = DateTime.Now;
+            string targetNameTimestampNew = dtNewDate.ToString("yyyyMMdd");
+
+
+
+            // Set the correct increment number depending on the other targetNameRoots in this directory
+            // Search directory for same name and timestamp
+            // unused approach but here for ref: https://stackoverflow.com/questions/8443524/using-directory-getfiles-with-a-regex-in-c
+            string targetNameRootAndTimestampNew = targetNameRoot + '_' + targetNameTimestampNew;
+            DebugOutput("targetNameRootAndTimestampNew:" + targetNameRootAndTimestampNew);
+            string[] matchPathsFound;
+            if (ftype == FTYPE.DIR)
+            {
+                matchPathsFound = Directory.GetDirectories(targetDirName, targetNameRootAndTimestampNew + '*', SearchOption.TopDirectoryOnly);
+            }
+            else
+            {
+                matchPathsFound = Directory.GetFiles(targetDirName, targetNameRootAndTimestampNew + '*', SearchOption.TopDirectoryOnly);
+            }
+
+            int foundIncrementMax = 0; // Start out with assumed max increment number.  This is the stored record
+            if (matchPathsFound.Length > 0) // If found matching names in this directory with same timestamp
+            {
+                DebugOutput("Found matches in directory:");
+                foreach (string f in matchPathsFound)
+                {
+                    DebugOutput(f);
+                }
+
+                // Enhance search by scanning for increment number if present
+                Regex regexSameNameInDirectory = new Regex(targetNameRootAndTimestampNew + @"_(\d+)");
+                
+                // Inspect each match to find highest increment number
+                foreach(string matchPathFound in matchPathsFound)
+                {
+                    Match matchSameNameInDirectory = regexSameNameInDirectory.Match(matchPathFound);
+                    int foundIncrement;
+                    if (matchSameNameInDirectory.Success)
+                    {
+                        foundIncrement = Convert.ToInt32(matchSameNameInDirectory.Groups[1].ToString());
+                        DebugOutput("Found Incremented Name w num: " + foundIncrement.ToString());
+                    }
+                    else
+                    {
+                        foundIncrement = 1; //  Any unincremented files are treated as having an increment number of 1
+                        DebugOutput("Found Unincremented Names:" + matchPathFound);
+                    }
+
+                    // If the increment number beats the record
+                    if(foundIncrement > foundIncrementMax)
+                    {
+                        // Store the new record
+                        foundIncrementMax = foundIncrement;
+                    }
+
+                }
+                DebugOutput("Max Increment Found:" + foundIncrementMax);
+                
+            }
+            else
+            {
+                DebugOutput("No matches found in directory");
+            }
+
+            // Determine this increment number
+            string targetNameIncrementNew = Convert.ToString(foundIncrementMax + 1);
+            DebugOutput("targetNameIncrementNew:" + targetNameIncrementNew);
 
 
 
 
-            //// Make new path
-            //string newPath = origPath + "_" + strLocalDate;
+            // Add Increment number to targetNameNew
+            string targetNameNew = targetNameRoot + '_' + targetNameTimestampNew + '_' + targetNameIncrementNew;
+            DebugOutput("targetNameNew:" + targetNameNew);
 
-            ////// For debug
-            ////Console.Write("New Path is: \t" + newPath + '\t');
-            ////foreach (char c in newPath)
-            ////{
-            ////    Console.Write(Convert.ToInt32(c) + " ");
-            ////}
-            ////Console.WriteLine();
 
+            // Construct Entire new path
+            // add backslash if needed
+            string targetPathNew = targetDirName + targetNameNew + targetExt;
+            DebugOutput("targetPathNew:" + targetPathNew);
 
 
 
-            //// https://stackoverflow.com/questions/1395205/better-way-to-check-if-a-path-is-a-file-or-a-directory
-            //// get the file attributes for file or directory
-            //FileAttributes attr = File.GetAttributes(origPath);
 
-            ////detect whether its a directory or file
-            //if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
-            //    //MessageBox.Show("Its a directory");
-            //    System.IO.Directory.Move(origPath, newPath);
+
+
+            //// Rename file using Move() method
+            //if (ftype == FTYPE.DIR)
+            //{
+            //    System.IO.Directory.Move(targetPathPrev, newPath);
+            //}
             //else
-            //    //MessageBox.Show("Its a file");
-            //    System.IO.File.Move(origPath, newPath);
+            //{
+            //    System.IO.File.Move(targetPathPrev, newPath);
+            //}
+
 
 
 
             Console.ReadKey();
+        }
+
+        public static void DebugOutput(string s)
+        {
+            if (DEBUGGING)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
